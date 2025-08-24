160b17b277fb76e55a7503e387dac1addad42966
seu-Erbeson <erbeson311991@gmail.com>
2025-08-23 22:08:15 -0400

    FIX: Adiciona urls.py para a app de usuarios

--- backend/usuarios/urls.py
+++ backend/usuarios/urls.py
@@ -1,13 +1,12 @@
-# backend/usuarios/urls.py - VERSÃO ATUALIZADA
+# backend/usuarios/urls.py
 
 from django.urls import path
-from .views import RegistroView, CurrentUserView # Importamos nossa nova view
+from .views import RegistroView, CurrentUserView
 
 urlpatterns = [
     # Rota para criar um novo usuário (pública)
     path('registro/', RegistroView.as_view(), name='registro'),
 
     # Rota para buscar os dados do usuário logado (protegida)
-    # É esta que o nosso frontend vai chamar
-    path('users/me/', CurrentUserView.as_view(), name='current-user'),
+    path('me/', CurrentUserView.as_view(), name='current-user'),
 ]
