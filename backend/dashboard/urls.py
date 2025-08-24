4fa15887908ad9d51288eb5571bb774a618ce5a0
seu-Erbeson <erbeson311991@gmail.com>
2025-08-23 20:45:57 -0400

    FIX: Adiciona urls.py para a app de dashboard

--- backend/dashboard/urls.py
+++ backend/dashboard/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from .views import DashboardSummaryView
 
 urlpatterns = [
+    # A URL completa será /api/dashboard/summary/
     path('summary/', DashboardSummaryView.as_view(), name='dashboard-summary'),
 ]
