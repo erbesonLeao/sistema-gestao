b49ec855eebde673ac47396c3913c3350851a06f
seu-Erbeson <erbeson311991@gmail.com>
2025-08-23 20:34:53 -0400

    FIX: Adiciona urls.py para a app de estoque

--- backend/estoque/urls.py
+++ backend/estoque/urls.py
@@ -2,10 +2,9 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProdutoViewSet # Importamos a ViewSet de produtos
+from .views import ProdutoViewSet 
 
 router = DefaultRouter()
-# O modelo principal da app de estoque provavelmente é 'Produto', então a ViewSet deve ter esse nome.
 router.register(r'', ProdutoViewSet, basename='produto')
 
 urlpatterns = [
